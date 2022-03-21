b8e8218123dbe7f7f098a156eb3139f2d0f273f9
Ylian Saint-Hilaire <ysainthilaire@hotmail.com>
2022-03-21 09:50:47 -0700

    Removed support for old basic/digest authenticaiton for redirection connections.

--- amt-redir-node-0.1.0.js
+++ amt-redir-node-0.1.0.js
@@ -184,6 +184,7 @@ var CreateAmtRedirect = function (module) {
                                 // Good Digest Auth (With cnonce and all)
                                 obj.xxSend(String.fromCharCode(0x13, 0x00, 0x00, 0x00, 0x04) + IntToStrX(obj.user.length + obj.authuri.length + 8) + String.fromCharCode(obj.user.length) + obj.user + String.fromCharCode(0x00, 0x00) + String.fromCharCode(obj.authuri.length) + obj.authuri + String.fromCharCode(0x00, 0x00, 0x00, 0x00));
                             }
+                            /*
                             else if (authData.indexOf(3) >= 0) {
                                 // Bad Digest Auth (Not sure why this is supported, cnonce is not used!)
                                 obj.xxSend(String.fromCharCode(0x13, 0x00, 0x00, 0x00, 0x03) + IntToStrX(obj.user.length + obj.authuri.length + 7) + String.fromCharCode(obj.user.length) + obj.user + String.fromCharCode(0x00, 0x00) + String.fromCharCode(obj.authuri.length) + obj.authuri + String.fromCharCode(0x00, 0x00, 0x00));
@@ -192,6 +193,7 @@ var CreateAmtRedirect = function (module) {
                                 // Basic Auth (Probably a good idea to not support this unless this is an old version of Intel AMT)
                                 obj.xxSend(String.fromCharCode(0x13, 0x00, 0x00, 0x00, 0x01) + IntToStrX(obj.user.length + obj.pass.length + 2) + String.fromCharCode(obj.user.length) + obj.user + String.fromCharCode(obj.pass.length) + obj.pass);
                             }
+                            */
                             else obj.Stop(2);
                         // ###BEGIN###{Mode-NodeWebkit}
                         }
